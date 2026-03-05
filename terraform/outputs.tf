646940c3c704c06ac72d3a783b171309e856da43
Sana Siddiqui <sana.siddiqui115@gmail.com>
2026-03-05 15:28:05 +0100

    Add encryption output to test full optimized pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "bucket_arn" {
   description = "ARN of the logs bucket"
   value       = aws_s3_bucket.logs.arn
 }
+
+output "bucket_encryption" {
+  description = "Encryption algorithm used"
+  value       = "AES256"
+}
